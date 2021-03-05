cfd6dda62374e71c7e76aed9f5abbff5e4eaf432
Sebery <seberyincorporation@gmail.com>
2021-03-04 19:30:25 -0600

    Main Setup

---
+++ docusaurus.config.js
@@ -0,0 +1,42 @@
+module.exports = {
+  title: 'Joint Docs',
+  tagline: 'The tagline of my site',
+  url: 'https://your-docusaurus-test-site.com',
+  baseUrl: '/',
+  onBrokenLinks: 'throw',
+  onBrokenMarkdownLinks: 'warn',
+  favicon: 'img/favicon.ico',
+  organizationName: 'Sebery', // Usually your GitHub org/user name.
+  projectName: 'Joint-Docs', // Usually your repo name.
+  themeConfig: {
+    navbar: {
+      title: 'JOINT DOCUMENTATION',
+      logo: {
+        alt: 'My Site Logo',
+        src: 'img/logo.svg',
+      },
+    },
+    footer: {
+      style: 'dark',
+      copyright: `Copyright © ${new Date().getFullYear()} Joint.`,
+    },
+  },
+
+  presets: [
+    [
+      '@docusaurus/preset-classic',
+      {
+        docs: {
+          sidebarPath: require.resolve('./sidebars.js'),
+          // Please change this to your repo.
+          editUrl:
+            'https://github.com/Sebery/Joint-Docs',
+            routeBasePath: '/',
+        },
+        theme: {
+          customCss: require.resolve('./src/css/custom.css'),
+        },
+      },
+    ],
+  ],
+};
